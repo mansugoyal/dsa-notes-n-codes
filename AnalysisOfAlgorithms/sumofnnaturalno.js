5c4968910e33de6b35ad2205c92295e2c824da31
mansugoyal <mansugoyal1234@gmail.com>
2025-08-15 13:20:28 +0530

    sumofnnaturalno

--- AnalysisOfAlgorithms/sumofnnaturalno.js
+++ AnalysisOfAlgorithms/sumofnnaturalno.js
@@ -1,16 +1,17 @@
 // input n= 3 and output should be 6
 
 //method 1: using recursion
-const n = 3
-function sumOfNNaturalNumbers(n) {
-    if (n <= 0) {
+const m = 3;
+function sumOfNNaturalNumbers(m) {
+    if (m <= 0) {
         return 0;
     }
-    return n + sumOfNNaturalNumbers(n - 1);
+    return m + sumOfNNaturalNumbers(m - 1);
 }
-console.log(sumOfNNaturalNumbers(n));
+console.log(sumOfNNaturalNumbers(m));
 
 // method 2: using loop
+const n = 5;
 let sum = 0;
 for(let i=1; i<=n; i++) {
     sum += i;
