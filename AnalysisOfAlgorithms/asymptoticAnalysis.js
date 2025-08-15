73ff4143be663173f86252c3175f40e3a93df6ff
mansugoyal <mansugoyal1234@gmail.com>
2025-08-15 15:17:53 +0530

    asymptoticAnalysis

---
+++ AnalysisOfAlgorithms/asymptoticAnalysis.js
@@ -0,0 +1,17 @@
+/*1. What is Asymptotic Analysis?
+
+Asymptotic Analysis is a way to measure the efficiency of an algorithm as the size of the input (n) grows very large.
+
+It ignores:
+
+Machine speed
+
+Programming language
+
+Constant factors
+
+It focuses only on how the algorithm’s runtime (or memory usage) grows compared to the size of input.
+
+Think of it as:
+
+"How does the algorithm behave when n becomes huge?"*/
